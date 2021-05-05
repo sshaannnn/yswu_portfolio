b9ac06cefb4c6cafd0cb468bb12fa8ec841bec57
sshaannnn <shan87826@gmail.com>
2021-05-05 16:39:50 +0800

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+
+const routes: Routes = [
+  {
+    path: 'Portfolio',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    redirectTo: '/Portfolio',
+    pathMatch: 'full'
+  },
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { HomeModule } from './home/home.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
