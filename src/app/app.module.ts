dfad90ab9c903219117285075d168d48a06a64ca
sshaannnn <shan87826@gmail.com>
2021-05-06 09:26:09 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
-import { BrowserModule, Title } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HomeModule,
   ],
-  providers: [Title],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constructor(
-    private titleService: Title) {
-    this.titleService.setTitle('YuShan | Creative Resume');
+  constructor() {
+
   }
 
   ngOnInit(): void {
